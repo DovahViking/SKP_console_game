4df71b04b3c61e0ed1f0705f570f5d9450b95a9f
Dovah Viking <DovahViking@hotmail.com>
2020-03-23 10:28:14 +0100

    Fully functional player movement in console + bugs

--- Bullet_Hell/Player.cs
+++ Bullet_Hell/Player.cs
@@ -7,7 +7,7 @@ namespace Bullet_Hell
     public class Player
     {
         public int player_position_x { get; set; } = 25;
-        public int player_position_y { get; set; } = 0;
+        public int player_position_y { get; set; } = -18;
         public int speed { get; set; } = 0;
         public string character { get; set; } = "|__uwu__|";
 
@@ -19,7 +19,12 @@ namespace Bullet_Hell
             this.character = character;
         }
 
-        public void input()
+        public void collision()
+        {
+
+        }
+
+        public void input() // For some reason, the player_position_x = 0 starts at like x position 20. So the maximum range for x is really something like 70 *shrug*
         {
             bool quit = false;
             while (quit == false)
@@ -47,23 +52,47 @@ namespace Bullet_Hell
                 }
                 if (key_input.Key == ConsoleKey.UpArrow || key_input.Key == ConsoleKey.W)
                 {
-                    player_position_y += 1;
-                    Console.WriteLine("W");
+                    player_position_y -= 1; // ------------------------------------------------------------------------------ Needs fixing
+                    if (player_position_y <= 0)
+                    {
+                        player_position_y += 1;
+                    }
+                    Console.Clear();
+                    Console.SetCursorPosition((25 - character.Length) / 2 + player_position_y + player_position_x, Console.CursorTop = player_position_y);
+                    Console.WriteLine(character);
                 }
                 if (key_input.Key == ConsoleKey.LeftArrow || key_input.Key == ConsoleKey.A)
                 {
                     player_position_x -= 1;
-                    Console.WriteLine("A");
+                    if (player_position_x <= 0)
+                    {
+                        player_position_x += 1;
+                    }
+                    Console.Clear();
+                    Console.SetCursorPosition((25 - character.Length) / 2 + player_position_x + player_position_y, Console.CursorTop + player_position_y);
+                    Console.WriteLine(character);
                 }
                 if (key_input.Key == ConsoleKey.DownArrow || key_input.Key == ConsoleKey.S)
                 {
-                    player_position_y -= 1;
-                    Console.WriteLine("S");
+                    player_position_y += 1; // ------------------------------------------------------------------------------ Needs fixing
+                    if (player_position_y >= 20)
+                    {
+                        player_position_y -= 1;
+                    }
+                    Console.Clear();
+                    Console.SetCursorPosition((25 - character.Length) / 2 + player_position_y + player_position_x, Console.CursorTop = player_position_y);
+                    Console.WriteLine(character);
                 }
                 if (key_input.Key == ConsoleKey.RightArrow || key_input.Key == ConsoleKey.D)
                 {
                     player_position_x += 1;
-                    Console.WriteLine("D");
+                    if (player_position_x >= 50)
+                    {
+                        player_position_x -= 1;
+                    }
+                    Console.Clear();
+                    Console.SetCursorPosition((25 - character.Length) / 2 + player_position_x + player_position_y, Console.CursorTop + player_position_y);
+                    Console.WriteLine(character);
                 }
             }
         }
